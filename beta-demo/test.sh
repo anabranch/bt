84885e5e41717c7ff051424eb18b26f664bd1633
bill_chambers <bill@anyscale.com>
2020-11-19 13:54:41 -0800

    fix

--- beta-demo/test.sh
+++ beta-demo/test.sh
@@ -1,7 +1,8 @@
-echo date
+date
 anyscale up --config=minimal_no_gpu.yaml --disable-sync test-session
-echo date
+sleep 30
+date
 for i in {1..20}; do anyscale exec -n test-session 'python -c "import ray; ray.init();print(ray.available_resources())" \n'; date ; sleep 30; done
-echo date
+date
 anyscale down --terminate test-session
-echo date
+date
