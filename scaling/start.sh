edf6a48d3eaa34fad8fdbb6d9838c21dcea4ac2e
will <will@anyscale.com>
2020-12-08 10:36:36 -0600

    Improved start script

--- scaling/start.sh
+++ scaling/start.sh
@@ -1,3 +1,12 @@
-anyscale up test-scaling \
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo $0: usage: sh start.sh sessionname
+    exit 1
+fi
+
+SESSION_NAME=$1
+
+anyscale up $SESSION_NAME \
     --config cluster.yaml \
     --cloud-name anyscale_default_cloud
